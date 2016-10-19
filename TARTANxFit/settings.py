db7152352af6231b41d35a88bad3f6a6126b3363
Jennifer Jin <jj1@andrew.cmu.edu>
2016-10-19 02:11:43 -0400

    bootstrap working, basic index page

--- TARTANxFit/settings.py
+++ TARTANxFit/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'TxFApp',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
--- TxFApp/views.py
+++ TxFApp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import * 
 
 def home(request):
     context = {}
+    context['classes'] = Class.objects.all()
     return render(request, 'TxFApp/index.html', context)
