7b06101929c9244ac621d2714148b6223e719535
Maggie Yu <maggieyu@cmu.edu>
2016-11-02 03:40:54 -0400

    Facebook log out and log in working

--- TxFApp/static/js/application.js
+++ TxFApp/static/js/application.js
@@ -13,6 +13,7 @@ $(function() {
 	});
 });
 
+// PushCrew notification
 (function(p,u,s,h){
     p._pcq=p._pcq||[];
     p._pcq.push(['_currentTime',Date.now()]);
--- TxFApp/static/js/facebook.js
+++ TxFApp/static/js/facebook.js
@@ -1,6 +1,5 @@
 // This is called with the results from from FB.getLoginStatus().
 function statusChangeCallback(response) {
-  console.log('statusChangeCallback');
   console.log(response);
   // The response object is returned with a status field that lets the
   // app know the current login status of the person.
@@ -9,16 +8,17 @@ function statusChangeCallback(response) {
   if (response.status === 'connected') {
     // Logged into your app and Facebook.
     testAPI();
-    location.assign("/schedule"); 
+    // Redirect only if you are on the login page and just logged in
+    if (window.location == "http://localhost:8000/login") {
+      location.assign("/schedule"); 
+    }
   } else if (response.status === 'not_authorized') {
     // The person is logged into Facebook, but not your app.
-    document.getElementById('status').innerHTML = 'Please log ' +
-      'into this app.';
+    console.log("not authorized");
   } else {
     // The person is not logged into Facebook, so we're not sure if
     // they are logged into this app or not.
-    document.getElementById('status').innerHTML = 'Please log ' +
-      'into Facebook.';
+    console.log("not logged in");
   }
 }
 
@@ -32,29 +32,29 @@ function checkLoginState() {
 }
 
 window.fbAsyncInit = function() {
-FB.init({
-  appId      : '355051971506326',
-  cookie     : true,  // enable cookies to allow the server to access 
-                      // the session
-  xfbml      : true,  // parse social plugins on this page
-  version    : 'v2.8' // use graph api version 2.5
-});
+  FB.init({
+    appId      : '355051971506326',
+    cookie     : true,  // enable cookies to allow the server to access 
+                        // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.8' // use graph api version 2.5
+  });
 
-// Now that we've initialized the JavaScript SDK, we call 
-// FB.getLoginStatus().  This function gets the state of the
-// person visiting this page and can return one of three states to
-// the callback you provide.  They can be:
-//
-// 1. Logged into your app ('connected')
-// 2. Logged into Facebook, but not your app ('not_authorized')
-// 3. Not logged into Facebook and can't tell if they are logged into
-//    your app or not.
-//
-// These three cases are handled in the callback function.
+  // Now that we've initialized the JavaScript SDK, we call 
+  // FB.getLoginStatus().  This function gets the state of the
+  // person visiting this page and can return one of three states to
+  // the callback you provide.  They can be:
+  //
+  // 1. Logged into your app ('connected')
+  // 2. Logged into Facebook, but not your app ('not_authorized')
+  // 3. Not logged into Facebook and can't tell if they are logged into
+  //    your app or not.
+  //
+  // These three cases are handled in the callback function.
 
-FB.getLoginStatus(function(response) {
-  statusChangeCallback(response);
-});
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
 
 };
 
@@ -73,7 +73,12 @@ function testAPI() {
   console.log('Welcome!  Fetching your information.... ');
   FB.api('/me', function(response) {
     console.log('Successful login for: ' + response.name);
-    document.getElementById('status').innerHTML =
-      'Thanks for logging in, ' + response.name + '!';
+  });
+}
+
+// Log out of Facebook when user clicks log out button
+function facebookLogout() {
+  FB.logout(function(response) {
+    console.log("Facebook logged out");
   });
 }
